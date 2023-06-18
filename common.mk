25f896032d430b542e845f3af42787466b516bd4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-18 16:35:32 +0400

    sm6225-common: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * use non-debug dex2oat
    
    Change-Id: Ic310df1e2c466437b26fcb2e5ab9333694328c48
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- common.mk
+++ common.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
