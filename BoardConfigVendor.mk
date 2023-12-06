da4a4bd18ff5be83238be3cb709bdfacd1638fd3
Michael Bestas <mkbestas@lineageos.org>
2023-12-06 17:02:34 +0200

    devon: Import firmware from T2SN33.73-22-3
    Change-Id: I3e499cebf781606714ec3c11021fe3513a9e8f20

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,22 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    bluetooth \
+    modem \
+    abl \
+    devcfg \
+    dsp \
+    fsg \
+    hyp \
+    keymaster \
+    logo \
+    prov \
+    qupfw \
+    rpm \
+    storsec \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
