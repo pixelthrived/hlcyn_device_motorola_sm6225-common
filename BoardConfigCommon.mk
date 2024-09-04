8075c83db7727a1372d457d1e5c2d75b9edba386
Kumar Anand <kumaranand@google.com>
2024-09-05 04:20:53 +0530

    sm6225-common: Enable automatic channel selection
    * Automatic Channel Selection algorithms and implementations
      are used to enable interfaces to automatically figure
      out which channel configuration to use for initiating
      communication, for any mode of operation which initiates
      radiation (AP, Mesh, IBSS, P2P).
    
      https://wireless.wiki.kernel.org/en/users/documentation/acs
    
    Change-Id: I2b20d634ba015f0e195b2d71ac22acee20a7fccd
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,6 +193,7 @@ WIFI_DRIVER_STATE_ON := "ON"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+CONFIG_ACS := true
 
 # Inherit from the proprietary version
 include vendor/motorola/sm6225-common/BoardConfigVendor.mk
