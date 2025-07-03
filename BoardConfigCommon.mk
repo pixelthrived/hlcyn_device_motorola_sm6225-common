dd4e6a513fd439c31e9575448590e810212fa822
zmafnan <@afnan_zikri>
2025-07-03 09:12:22 -0400

    sm6225-common: Better RAM Management
    - Caprip only has two versions 4GB and 6GB, it's helpful for the most of people who has the 4GB version and in general.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/class/sensors/dt-gesture/enable"
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
