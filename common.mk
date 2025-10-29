67c0216ef983f2456ede11c08e32a1c4071a4aff
PixelThrived <pixelthrived@gmail.com>
2025-10-29 17:07:12 +0200

    sm6225-common: Include signing keys
    Change-Id: I75d05b1724bb172df0a147192f498cbc3f3e1efd
    Signed-off-by: PixelThrived <pixelthrived@gmail.com>

--- common.mk
+++ common.mk
@@ -6,6 +6,8 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Include signing keys if they exist
+-include vendor/lineage-priv/keys/keys.mk
 
 # Enable whole-program R8 Java optimizations for system_server.
 FULL_SYSTEM_OPTIMIZE_JAVA := true
